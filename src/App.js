745a55e1244fb77f5478024fa2831a1556c2c24b
Abhijeet Patil <abhijeetpatil032005@gmail.com>
2025-04-21 16:03:34 +0530

    Charts implented and Theme Toggle Implemented

--- src/App.js
+++ src/App.js
@@ -27,10 +27,10 @@ import { useStateContext } from "./contexts/ContextProvider";
 
 import "./App.css";
 const App = () => {
-  const {activeMenu} = useStateContext()
+  const {activeMenu,themeSettings,setThemeSettings,currentColor,currentMode} = useStateContext()
 
   return (
-    <div>
+    <div className={currentMode==="Dark" ? 'dark' : ''}>
       <BrowserRouter>
         <div className="flex relative dark:bg-main-dark-bg">
           <div className="fixed right-4 bottom-4" style={{ zIndex: "1000" }}>
@@ -38,7 +38,8 @@ const App = () => {
               <button
                 type="button"
                 className="text-3xl p-3 hover:drop-shadow-xl hover:bg-light-gray text-white"
-                style={{ background: "blue", borderRadius: "50%" }}
+                onClick={()=>setThemeSettings(true)}
+                style={{ background: currentColor, borderRadius: "50%" }}
               >
                 <FiSettings />
               </button>
@@ -54,8 +55,10 @@ const App = () => {
             </div>
           )}
           <div
-            className={`dark:bg-main-bg bg-main-bg min-h-screen w-full ${
-              activeMenu ? "md:ml-72 " : "flex-2"
+            className={`dark:bg-main-dark-bg bg-main-bg min-h-screen w-full 
+              ${activeMenu 
+                ? "md:ml-72 " 
+                : "flex-2"
             }`}
           >
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
@@ -63,6 +66,8 @@ const App = () => {
             </div>
           
           <div>
+            {themeSettings && <ThemeSettings/>}
+
             <Routes>
               {/* Dashboard */}
               <Route path="/" element={<Ecommerce />} />
--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -15,6 +15,25 @@ export const ContextProvider = ({ children }) => {
   const [isClicked, setIsClicked] = useState(initialState);
 
   const [screeSize,setScreenSize] = useState(undefined)
+  const [currentColor,setCurrentColor] = useState('#03C9D7')
+  const [currentMode,setCurrentMode] = useState('Light')
+  const [themeSettings,setThemeSettings] = useState(false)
+
+  const setMode = (e)=>{
+    setCurrentMode(e.target.value)
+
+    localStorage.setItem('themeMode',e.target.value)
+
+    setThemeSettings(false)
+  }
+  const setColor = (color)=>{
+    setCurrentColor(color)
+
+    localStorage.setItem('colorMode',color)
+
+    setThemeSettings(false)
+  }
+
   const handleClick = (clicked) => {
     setIsClicked({ ...initialState, [clicked]: true });
   };
@@ -27,7 +46,13 @@ export const ContextProvider = ({ children }) => {
         setIsClicked,
         handleClick,
         screeSize,
-        setScreenSize
+        setScreenSize,
+        currentColor,
+        currentMode,
+        themeSettings,
+        setThemeSettings,
+        setMode,
+        setColor
       }}
     >
       {children}
