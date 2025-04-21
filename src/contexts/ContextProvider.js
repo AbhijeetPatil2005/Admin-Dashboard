7fd9a05112323768dfb66f5c5362bbf5a89968d1
Abhijeet Patil <abhijeetpatil032005@gmail.com>
2025-04-21 16:26:49 +0530

    Navbar tools implemented

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -1,5 +1,4 @@
-import React, { createContext, useContext, useState } from "react";
-import { UserProfile } from "../components";
+import React, { createContext, useContext, useState } from 'react';
 
 const StateContext = createContext();
 
@@ -11,50 +10,28 @@ const initialState = {
 };
 
 export const ContextProvider = ({ children }) => {
+  const [screenSize, setScreenSize] = useState(undefined);
+  const [currentColor, setCurrentColor] = useState('#03C9D7');
+  const [currentMode, setCurrentMode] = useState('Light');
+  const [themeSettings, setThemeSettings] = useState(false);
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
 
-  const [screeSize,setScreenSize] = useState(undefined)
-  const [currentColor,setCurrentColor] = useState('#03C9D7')
-  const [currentMode,setCurrentMode] = useState('Light')
-  const [themeSettings,setThemeSettings] = useState(false)
-
-  const setMode = (e)=>{
-    setCurrentMode(e.target.value)
-
-    localStorage.setItem('themeMode',e.target.value)
-
-    setThemeSettings(false)
-  }
-  const setColor = (color)=>{
-    setCurrentColor(color)
-
-    localStorage.setItem('colorMode',color)
-
-    setThemeSettings(false)
-  }
-
-  const handleClick = (clicked) => {
-    setIsClicked({ ...initialState, [clicked]: true });
+  const setMode = (e) => {
+    setCurrentMode(e.target.value);
+    localStorage.setItem('themeMode', e.target.value);
   };
+
+  const setColor = (color) => {
+    setCurrentColor(color);
+    localStorage.setItem('colorMode', color);
+  };
+
+  const handleClick = (clicked) => setIsClicked({ ...initialState, [clicked]: true });
+
   return (
-    <StateContext.Provider
-      value={{
-        activeMenu,
-        setActiveMenu,
-        isClicked,
-        setIsClicked,
-        handleClick,
-        screeSize,
-        setScreenSize,
-        currentColor,
-        currentMode,
-        themeSettings,
-        setThemeSettings,
-        setMode,
-        setColor
-      }}
-    >
+    // eslint-disable-next-line react/jsx-no-constructed-context-values
+    <StateContext.Provider value={{ currentColor, currentMode, activeMenu, screenSize, setScreenSize, handleClick, isClicked, initialState, setIsClicked, setActiveMenu, setCurrentColor, setCurrentMode, setMode, setColor, themeSettings, setThemeSettings }}>
       {children}
     </StateContext.Provider>
   );
